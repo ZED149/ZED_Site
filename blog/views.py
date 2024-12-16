33af60167ef2e0fe80b7e330bb21ba7c9f43b6e7
Salman Ahmad <salmanahmad111499@gmail.com>
2024-12-16 16:34:20 +0500

    added empty views for the blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# home
+def home(request):
+    return HttpResponse("Salman Ahmad")
+
+
+# posts
+def posts(request):
+    return HttpResponse("Posts")
+
+
+# posts_by_slug
+def posts_by_slug(request, slug):
+    return HttpResponse(slug)
