0071e036739c97360d8984fa5035d49f597422cf
Salman Ahmad <salmanahmad111499@gmail.com>
2024-12-16 16:41:52 +0500

    added empty templates and css for the blog

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("posts/", views.posts, name="posts"),
-    path("posts/<str:slug>", views.posts_by_slug, name="posts_slug")
+    path("posts/<slug:slug>", views.posts_by_slug, name="posts_slug")
 ]
