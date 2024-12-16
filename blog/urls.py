e3cd4d09958cc3bdb934bb22e3dceeaaeb1ad125
Salman Ahmad <salmanahmad111499@gmail.com>
2024-12-16 16:30:29 +0500

    added urlpatterns in blog.urls and registered them in main site urls.py

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("posts/", views.posts, name="posts"),
+    path("posts/<str:slug>", views.posts_by_slug, name="posts_slug")
+]
