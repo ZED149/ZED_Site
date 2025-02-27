048dae56e3cd475ab7a42e4911f193881d2bf168
Salman Ahmad <salmanahmad111499@gmail.com>
2025-02-27 13:47:09 +0500

    added data to admin panel and tweaked some settings

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,10 @@ from django.contrib import admin
 
 from .models import Post, Author, Tag
 
-admin.site.register(Post)
+# PostAdmin
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,19 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.caption}"
+    
+
+# Author Class
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 # Post Class
 class Post(models.Model):
@@ -17,13 +30,9 @@ class Post(models.Model):
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     tags = models.ManyToManyField(Tag)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    
 
-
-# Author Class
-class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    posts = models.ForeignKey(Post, on_delete=models.SET_NULL, related_name="posts", null=True)
-
+    def __str__(self):
+        return f"{self.title}"
 
