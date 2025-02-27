59e025332602f21a14806a77dfc41c250645d047
Salman Ahmad <salmanahmad111499@gmail.com>
2025-02-27 14:11:16 +0500

    added filters for admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,13 @@ from .models import Post, Author, Tag
 # PostAdmin
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "tags")
+
+
+# AuthorAdmin
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "email",)
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Tag)
